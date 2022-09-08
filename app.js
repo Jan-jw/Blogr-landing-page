6da2c7f604a8bd2221a4f7f53afb209cb4469f7e
Jan-jw <74130412+Jan-jw@users.noreply.github.com>
2022-09-08 16:59:24 -0400

    formatting fix

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-
 // eventlistener for all dropdowns -> desktop +mobile
-document.querySelectorAll(".dropdown").forEach( e =>{
-    e.addEventListener("click", e => {
-        var svg = e.currentTarget.querySelector("svg");
-        var block = e.currentTarget.querySelector("ul");
-        console.log(e.currentTarget.parentElement);
-        console.log(svg.getAttribute("transform"));
-        if (svg.getAttribute("transform") === null || svg.getAttribute("transform") === "" ){
-            svg.setAttribute("transform", "rotate(180)");
-            block.style.display = "block";
-        } else{
-            svg.removeAttribute("transform");
-            block.style.display = "none";
-        }
-    })
+document.querySelectorAll(".dropdown").forEach((e) => {
+  e.addEventListener("click", (e) => {
+    var svg = e.currentTarget.querySelector("svg");
+    var block = e.currentTarget.querySelector("ul");
+    console.log(e.currentTarget.parentElement);
+    console.log(svg.getAttribute("transform"));
+    if (
+      svg.getAttribute("transform") === null ||
+      svg.getAttribute("transform") === ""
+    ) {
+      svg.setAttribute("transform", "rotate(180)");
+      block.style.display = "block";
+    } else {
+      svg.removeAttribute("transform");
+      block.style.display = "none";
+    }
+  });
 });
-
 
 let hamburger = document.querySelector(".hamburger");
 let cross = document.querySelector(".cross");
 let hamMenu = document.querySelector(".mobile-menu");
 
+hamburger.addEventListener("click", (e) => {
+  if (hamMenu.style.display === "none" || hamMenu.style.display === "") {
+    hamMenu.style.display = "flex";
+    hamburger.style.display = "none";
+    cross.style.display = "block";
+  }
+});
 
-hamburger.addEventListener("click", e =>{
-    if (hamMenu.style.display === "none"|| hamMenu.style.display === ""){
-        hamMenu.style.display = "flex";
-        hamburger.style.display = "none";
-        cross.style.display = "block";
-    }
-})
-
-cross.addEventListener("click", e =>{
-    if (hamMenu.style.display === "flex"){
-        hamMenu.style.display = "none";
-        cross.style.display = "none";
-        hamburger.style.display = "block";
-    }
-})
+cross.addEventListener("click", (e) => {
+  if (hamMenu.style.display === "flex") {
+    hamMenu.style.display = "none";
+    cross.style.display = "none";
+    hamburger.style.display = "block";
+  }
+});
