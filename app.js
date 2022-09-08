bd18af84c30bfd4b0be0f0204e99f7a6f1895e0f
Jan-jw <74130412+Jan-jw@users.noreply.github.com>
2022-09-08 16:52:34 -0400

    finalize design

--- app.js
+++ app.js
@@ -1,52 +1,38 @@
 // top-nav dropdowns click events
 
 const dropdownToggle = (e) =>{
-    // e.getElement
-}
-
-// eventlistener for all dropdowns - desktop
-document.querySelectorAll(".dropdown").forEach( e =>{
-    e.addEventListener("click", (e) =>{
-        //flip svg + toggle block
-        // console.log(e.currentTarget);
-
-        var svg = e.currentTarget.querySelector("svg");
+    var svg = e.currentTarget.querySelector("svg");
         var block = e.currentTarget.querySelector("ul");
-        console.log(block);
+        console.log(e.currentTarget.parentElement);
         console.log(svg.getAttribute("transform"));
-        // svg.setAttribute("transform", "rotate(180)");
         if (svg.getAttribute("transform") === null || svg.getAttribute("transform") === "" ){
             svg.setAttribute("transform", "rotate(180)");
             block.style.display = "block";
         } else{
             svg.removeAttribute("transform");
             block.style.display = "none";
-        }
+        }}
 
-    })
-}
-)
+// eventlistener for all dropdowns -> desktop +mobile
+document.querySelectorAll(".dropdown").forEach( e =>{
+    e.addEventListener("click", dropdownToggle(e))
+});
+
 
 let hamburger = document.querySelector(".hamburger");
 let cross = document.querySelector(".cross");
 let hamMenu = document.querySelector(".mobile-menu");
-// console.log(hamburger);
-// eventlistener for hamburger - mobile
-hamburger.addEventListener("click", e =>{
-    // let hamMenu = document.querySelector(".mobile-menu");
-    // console.log(hamMenu.style.display);
 
+
+hamburger.addEventListener("click", e =>{
     if (hamMenu.style.display === "none"|| hamMenu.style.display === ""){
-        // console.log("in")
         hamMenu.style.display = "flex";
         hamburger.style.display = "none";
         cross.style.display = "block";
-        // console.log(hamMenu.style.display);
     }
 })
 
 cross.addEventListener("click", e =>{
-    // let hamMenu = document.querySelector(".mobile-menu");
     if (hamMenu.style.display === "flex"){
         hamMenu.style.display = "none";
         cross.style.display = "none";
